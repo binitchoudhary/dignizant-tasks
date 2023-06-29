950df09d216941debcc284bc15e6834b1f587782
binitchoudhary <binitchoudhary1602@gmail.com>
2023-06-29 23:37:00 +0530

    added comments

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container mx-auto p-4">
+    <!-- Button to open the modal for creating a new post -->
     <button @click="openModal('create')" class="custom-button create-post">
       Create New Post
     </button>
+
+    <!-- Table to display the posts -->
     <table class="custom-table">
       <thead class="thead">
         <tr>
@@ -13,14 +16,18 @@
         </tr>
       </thead>
       <tbody>
+        <!-- Loop through the posts and display each post in a table row -->
         <tr v-for="post in paginatedPosts" :key="post.id" class="custom-tr">
           <td class="custom-td">{{ post.id }}</td>
           <td class="custom-td td-padding">{{ post.title }}</td>
           <td class="custom-td td-padding post-body">{{ post.body }}</td>
           <td class="custom-td btns">
+            <!-- Button to open the modal for editing a post -->
             <button @click="openModal('edit', post)" class="custom-button edit-btn">
               <i class="fas fa-edit"></i>
             </button>
+
+            <!-- Button to delete a post -->
             <button @click="deletePost(post.id)" class="custom-button delete-btn">
               <i class="fas fa-trash"></i>
             </button>
@@ -28,6 +35,8 @@
         </tr>
       </tbody>
     </table>
+
+    <!-- Pagination buttons -->
     <div class="pagination">
       <button @click="previousPage" :disabled="currentPage === 1" class="custom-button prev">
         Previous
@@ -37,6 +46,8 @@
         Next
       </button>
     </div>
+
+    <!-- Modal for creating/editing a post -->
     <div class="modal" id="postModal">
       <div class="modal-content">
         <h2 class="custom-heading">{{ modalTitle }}</h2>
@@ -50,9 +61,12 @@
             <textarea id="edit-body" v-model="editedPost.body" required class="custom-textarea"></textarea>
           </div>
           <div class="flex">
+            <!-- Button to create/save a post -->
             <button type="submit" class="custom-button create-save">
               {{ creatingPost ? 'Create' : 'Save' }}
             </button>
+
+            <!-- Button to cancel editing a post -->
             <button @click="cancelEdit" class="custom-button cancel">
               Cancel
             </button>
@@ -97,6 +111,7 @@ export default {
   methods: {
     openModal(mode, post = null) {
       if (mode === 'create') {
+        // Set modal state for creating a new post
         this.creatingPost = true;
         this.editingPost = false;
         this.modalTitle = 'Create New Post';
@@ -106,6 +121,7 @@ export default {
           body: '',
         };
       } else if (mode === 'edit') {
+        // Set modal state for editing an existing post
         this.creatingPost = false;
         this.editingPost = true;
         this.modalTitle = 'Edit Post';
@@ -172,48 +188,58 @@ export default {
   position: relative;
 }
 
+/* Styles for the create post button */
 .create-post {
   padding: 0.5rem;
   margin-bottom: 0.5rem;
   background: lightgreen;
-    border: 1px solid lightseagreen;
+  border: 1px solid lightseagreen;
 }
-button{
-  cursor: pointer;
-}
+
+/* Styles for the table */
 .custom-table {
   border-collapse: collapse;
   width: 100%;
 }
 
+/* Styles for table header */
 .custom-padding {
   padding: 0 2rem !important;
 }
 
+/* Styles for table cells with padding */
 .td-padding {
   padding: 0 1.5rem;
   text-align: left !important;
 }
 
+/* Styles for table header row */
 .thead {
   border: 1px solid black;
   padding: 8px;
   text-align: left;
   background: #CED2CC;
 }
+
+/* Styles for table body */
 tbody{
   background: gray;
   color: #fff;
 }
 
+/* Styles for table header cells */
 .custom-th {
   padding: 6px;
-background: blue;
-color: #fff;
+  background: blue;
+  color: #fff;
 }
+
+/* Styles for table rows */
 .custom-tr{
   color: black;
 }
+
+/* Styles for alternating table rows */
 .custom-tr:nth-child(even) {
   background-color: #f9f9f9;
 }
@@ -222,10 +248,12 @@ color: #fff;
   background-color: #ffffff;
 }
 
+/* Styles for table cells */
 .custom-td {
   text-align: center;
 }
 
+/* Styles for action buttons */
 .btns {
   display: flex;
   padding: 12px 0;
@@ -238,17 +266,24 @@ color: #fff;
   background: none;
   border: none;
 }
+
+/* Styles for delete button */
 .delete-btn{
   color: red;
 }
+
+/* Styles for edit button */
 .edit-btn{
   color: blue;
 }
+
+/* Styles for center alignment */
 .items-center {
   text-align: center;
   margin-top: 1rem;
 }
 
+/* Styles for pagination */
 .pagination {
   display: flex;
   justify-content: center;
@@ -256,19 +291,22 @@ color: #fff;
   margin: 1rem 0;
 }
 
+/* Styles for previous and next buttons in pagination */
 .prev,
 .next {
   padding: 0.5rem;
   margin: 0 0.5rem;
   background: #0cb130;
-    border: 1px solid lightgray;
+  border: 1px solid lightgray;
 }
 
+/* Styles for post body */
 .post-body {
   text-align: left;
   padding: 0 1rem;
 }
 
+/* Styles for modal */
 .modal {
   display: none;
   position: fixed;
@@ -281,6 +319,7 @@ color: #fff;
   background-color: rgba(0, 0, 0, 0.4);
 }
 
+/* Styles for modal content */
 .modal-content {
   background-color: #fefefe;
   margin: 10% auto;
@@ -291,10 +330,12 @@ color: #fff;
   background: #DADADA;
 }
 
+/* Styles for modal heading */
 .custom-heading {
   text-align: center;
 }
 
+/* Styles for form inside modal */
 .custom-form {
   display: flex;
   flex-direction: column;
@@ -302,11 +343,13 @@ color: #fff;
   margin-top: 1rem;
 }
 
+/* Styles for form label */
 .custom-label {
   padding-right: 20px;
   margin: 0.5rem;
 }
 
+/* Styles for form input and textarea */
 .custom-input,
 .custom-textarea {
   width: 100%;
@@ -314,35 +357,44 @@ color: #fff;
   height: 3rem;
 }
 
+/* Styles for create post title section */
 .create-title,
 .create-body {
   margin: 1rem 0;
 }
 
+/* Styles for create/save button */
 .create-save {
   margin: 1.5rem;
   padding: 0.5rem;
   background: green;
 }
 
+/* Styles for cancel button */
 .cancel {
   padding: 0.5rem;
   background: red;
 }
 
 @media (max-width: 640px) {
+  /* Responsive styles for smaller screens */
+
+  /* Styles for table cells with padding */
   .custom-padding {
     padding: 0 0.5rem !important;
   }
 
+  /* Styles for table cells */
   .td-padding {
     padding: 0 0.5rem;
   }
 
+  /* Styles for post body */
   .post-body {
     padding: 0;
   }
 
+  /* Styles for modal content */
   .modal-content {
     margin: 20% auto;
   }
